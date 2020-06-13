3fcf3fa7f03b9966753ad5937ae4b1ceb397d61d
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2020-06-13 11:52:01 -0600

    Add todo for toL

--- src/Numeric/LinearAlgebra/MatrixFree.hs
+++ src/Numeric/LinearAlgebra/MatrixFree.hs
@@ -27,6 +27,18 @@ data LinearMap (m :: Nat) (n :: Nat) where
 fromL :: (KnownNat m, KnownNat n) => L m n -> LinearMap m n
 fromL a = LinearMap (a #>) (tr a #>)
 
+-- TODO: Implement this!
 -- I'll need to think about how perform this map
+--   src/Numeric/LinearAlgebra/MatrixFree.hs:32:43: error:
+--     • Couldn't match type ‘Data.Vector.Storable.Vector’ with ‘[]’
+--         arising from a functional dependency between:
+--           constraint ‘Sized ℝ (R m) []’ arising from a use of ‘extract’
+--           instance ‘Sized ℝ (R n1) Data.Vector.Storable.Vector’
+--             at <no location info>
+--     • In the first argument of ‘(.)’, namely ‘extract’
+--       In the first argument of ‘concatMap’, namely ‘(extract . f)’
+--       In the second argument of ‘($)’, namely
+--         ‘concatMap (extract . f) (toColumns eye)’
+-- 
 -- toL :: (KnownNat m, KnownNat n) => LinearMap m n -> L m n
 -- toL (LinearMap f _) = matrix $ concatMap (extract . f) (toColumns eye)
