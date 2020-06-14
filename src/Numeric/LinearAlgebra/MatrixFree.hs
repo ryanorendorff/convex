393fd14d3e6059256de8ec93b6f4336de6e528fe
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2020-06-13 18:06:13 -0600

    Add toL function MatrixFree

--- src/Numeric/LinearAlgebra/MatrixFree.hs
+++ src/Numeric/LinearAlgebra/MatrixFree.hs
@@ -13,6 +13,7 @@
 module Numeric.LinearAlgebra.MatrixFree
     ( LinearMap(..)
     , fromL
+    , toL
     , eyeFree
     , avgRepeatedFree
     )
@@ -36,21 +37,11 @@ data LinearMap (m :: Nat) (n :: Nat) where
 fromL :: (KnownNat m, KnownNat n) => L m n -> LinearMap m n
 fromL a = LinearMap (a #>) (tr a #>)
 
--- TODO: Implement this!
--- I'll need to think about how perform this map
---   src/Numeric/LinearAlgebra/MatrixFree.hs:32:43: error:
---     • Couldn't match type ‘Data.Vector.Storable.Vector’ with ‘[]’
---         arising from a functional dependency between:
---           constraint ‘Sized ℝ (R m) []’ arising from a use of ‘extract’
---           instance ‘Sized ℝ (R n1) Data.Vector.Storable.Vector’
---             at <no location info>
---     • In the first argument of ‘(.)’, namely ‘extract’
---       In the first argument of ‘concatMap’, namely ‘(extract . f)’
---       In the second argument of ‘($)’, namely
---         ‘concatMap (extract . f) (toColumns eye)’
---
--- toL :: (KnownNat m, KnownNat n) => LinearMap m n -> L m n
--- toL (LinearMap f _) = matrix $ concatMap (extract . f) (toColumns eye)
+-- This is likely a terrible solution because of the conversion to list. We
+-- should be able to coerce the result safely!
+toL :: (KnownNat m, KnownNat n) => LinearMap m n -> L m n
+toL (LinearMap f _) =
+    matrix $ concatMap (V.toList . extract . f) (toColumns eye)
 
 
 eyeFree :: (KnownNat n) => LinearMap n n
