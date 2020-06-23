e462da14c16c3da42f79bd3d561035fec130276a
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2020-06-22 23:24:30 -0600

    Add more detailed comments Conversions{.Static}

--- src/ControlSystems/DynamicSystems/Conversions/Static.hs
+++ src/ControlSystems/DynamicSystems/Conversions/Static.hs
@@ -7,6 +7,9 @@
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
 
+-- The unused variables are left as documentation
+{-# OPTIONS -Wno-unused-local-binds #-}
+
 module ControlSystems.DynamicSystems.Conversions.Static (
   c2d
 ) where
@@ -14,13 +17,31 @@ module ControlSystems.DynamicSystems.Conversions.Static (
 import GHC.TypeLits
 import Numeric.LinearAlgebra.Static
 
--- Convert continuous time linear dynamical system to discrete time using a
--- given sampling period.
-c2d :: forall m n. (KnownNat m, KnownNat n) => Double -> L n n -> L n m -> (L n n, L n m)
-c2d t a b = splitCols ab
+-- | Convert a continuous time linear differential system to a discrete form,
+-- using state space models, using type level naturals.
+--
+-- Specifically note this implementation against
+-- `ControlSystems.DynamicSystems.Conversions.c2d`, which takes many more
+-- code to handle the complexities of creating a block matrix of the right
+-- size and then splitting it apart again.
+c2d :: forall m n. (KnownNat m, KnownNat n) =>
+       Double         -- ^ The sampling time
+    -> L n n          -- ^ The state transition matrix, often called A
+    -> L n m          -- ^ The state input matrix, often called B
+    -> (L n n, L n m) -- ^ The discretized A & B system
+c2d t a b = discreteSystem
   where
-    ab = fst (splitRows exp_block)
-    exp_block = expm (konst t * block)
-    block = a ||| b
-              ===
+    block = a ||| b  -- [ A B ]
+              ===    -- [ 0 0 ]
                0
+
+    -- Calculate the discrete time block matrix, which is the following
+    -- ⌈ A_d B_d ⌉
+    -- ⌊  0   I  ⌋
+    exp_block = expm (konst t * block)
+
+    -- Finally we can extract the correct submatrices assuming we pull out
+    -- the right pieces. Note that we do not need to pass the indices here
+    -- directly; the type system ensures that we pull out the correct A_d
+    -- and B_d parts of the exp_block matrix.
+    discreteSystem@(a_d, b_d) = splitCols $ fst (splitRows exp_block)
