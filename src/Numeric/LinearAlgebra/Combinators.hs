50e179ecbf60e9fea3d4d06babefd45845e875bc
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2020-06-22 23:38:00 -0600

    Whitespace Combinators.hs

--- src/Numeric/LinearAlgebra/Combinators.hs
+++ src/Numeric/LinearAlgebra/Combinators.hs
@@ -15,9 +15,9 @@ import           Numeric.LinearAlgebra.Static
 -- | Generate a block matrix from static matrices.
 blockMatrix
     :: (KnownNat m, KnownNat n, KnownNat p, KnownNat q)
-    => L m n -- ^ Upper left matrix
-    -> L m p -- ^ Upper right matrix
-    -> L q n -- ^ Lower left matrix
-    -> L q p -- ^ Lower right matrix
+    => L m n             -- ^ Upper left matrix
+    -> L m p             -- ^ Upper right matrix
+    -> L q n             -- ^ Lower left matrix
+    -> L q p             -- ^ Lower right matrix
     -> L (m + q) (n + p) -- ^ Resulting block matrix
 blockMatrix a b c d = a ||| b === c ||| d
