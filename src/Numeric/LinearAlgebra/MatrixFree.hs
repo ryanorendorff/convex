c118a85da930aad697a66fc870c3e1240d35ae2d
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2020-06-23 23:54:12 -0600

    Complete comments/docstrings for MatrixFree

--- src/Numeric/LinearAlgebra/MatrixFree.hs
+++ src/Numeric/LinearAlgebra/MatrixFree.hs
@@ -79,47 +79,49 @@ removeAvg = eyeFree +# ((-1) *# avgRepeatedFree)
 
 -- TODO: Define fft in matrix free form using FFT and IFFT
 
-trLM :: LinearMap n m -> LinearMap m n
-trLM (LinearMap f a) = LinearMap a f
-
 instance Transposable (LinearMap n m) (LinearMap m n) where
-    tr  = trLM
+   -- We do not have different functions for conjugate transposes in the
+   -- LinearMap constructor (yet?)
+    tr  (LinearMap f a) = LinearMap a f
     tr' = tr
 
 
--- Apply matrix free function to a vector
+-- | Apply matrix free LinearMap to a vector
 (##>) :: LinearMap n m -> R m -> R n
 (##>) (LinearMap f _) = f
 
 
 infixr 8 ##>
 
-(<#>)
-    :: LinearMap m n
-    -> LinearMap n p
-    -> LinearMap m p
+-- | Matrix multiply of two `LinearMap`s
+(<#>) :: LinearMap m n
+      -> LinearMap n p
+      -> LinearMap m p
 (<#>) (LinearMap f_a a_a) (LinearMap f_b a_b) =
     LinearMap (f_a . f_b) (a_b . a_a)
 
 infixr 8 <#>
 
 
+-- | Multiply a LinearMap matrix by a constant real number
 (*#) :: (KnownNat m, KnownNat n) => Double -> LinearMap m n -> LinearMap m n
 (*#) s (LinearMap f a) = LinearMap (\v -> konst s * f v) (\v -> konst s * a v)
 infixr 8 *#
 
 
-(+#)
-    :: (KnownNat m, KnownNat n)
-    => LinearMap m n
-    -> LinearMap m n
-    -> LinearMap m n
+-- | Add two `LinearMap`s together if they have the same shape.
+(+#) :: (KnownNat m, KnownNat n) =>
+        LinearMap m n
+     -> LinearMap m n
+     -> LinearMap m n
 (+#) (LinearMap f_a a_a) (LinearMap f_b a_b) =
     LinearMap (\v -> f_b v + f_a v) (\v -> a_b v + a_a v)
 
 infixr 7 +#
 
 
+-- | Useful for constraining two dependently typed LinearMap matrices to
+-- match each other in dimensions when they are unknown at compile-time.
 exactDimsFree
     :: forall m n j k.
        (KnownNat n, KnownNat m, KnownNat j, KnownNat k) =>
@@ -130,6 +132,9 @@ exactDimsFree (LinearMap f b) = do
     Refl <- sameNat (Proxy :: Proxy n) (Proxy :: Proxy k)
     return $ LinearMap f b
 
+-- | Useful for constraining that two dependently typed LinearMap matrices
+-- match in their inner dimensions (enabling matrix multiplication) each
+-- other in dimensions when they are unknown at compile-time.
 innerDimsFree
     :: forall m n p q.
        (KnownNat m, KnownNat n, KnownNat p, KnownNat q) =>
